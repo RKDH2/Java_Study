33bd5d7b2f622df9bd6d5548d736e0bfb514122c
rkdhs <gaons120@gmail.com>
2024-08-04 18:29:08 +0900

    Java Study

---
+++ src/section5/Main.java
@@ -0,0 +1,11 @@
+package section5;
+
+public class Main {
+    public static void main(String[] args) {
+        SelectMenu selectMenu = new SelectMenu();
+        selectMenu.Steak();
+        selectMenu.Dessert();
+        selectMenu.Drink();
+        selectMenu.Total();
+    }
+}
