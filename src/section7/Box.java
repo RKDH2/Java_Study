9f39cb603406a2eb9564e303c7907d487fa77565
rkdhs <gaons120@gmail.com>
2024-08-06 19:45:50 +0900

    Java Study

---
+++ src/section7/Box.java
@@ -0,0 +1,27 @@
+package section7;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box<T> {
+    List<T> list = new ArrayList<>();
+
+    public void add(T fruit) {
+        list.add(fruit);
+    }
+
+    public void remove(T fruit) {
+        list.remove(fruit);
+    }
+
+    public static void main(String[] args) {
+        // 제네릭 매개변수 <> 안에 정수형 타입을 할당
+        Box<Integer> integerBox = new Box<>();
+        // 제네릭 매개변수 <> 안에 실수형 타입을 할당
+        Box<Double> doubleBox = new Box<>();
+        // 제네릭 매개변수 <> 안에 문자열 타입을 할당
+        Box<String> stringBox = new Box<>();
+        // 제네릭 매개변수 <> 안에 클래스 할당 가능
+        // Box<newClass> newTypeBox = new Box<newClass>();
+    }
+}
